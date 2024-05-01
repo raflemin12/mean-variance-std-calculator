36ef7ee6533d89c7681ab4ba5b295193cfeee987
Ricky Fleming <raflemin@gmail.com>
2024-05-01 18:04:25 +0000

    updated calculate function to intake function object

--- mean_var_std.py
+++ mean_var_std.py
@@ -8,54 +8,19 @@ def calculate(list):
     array_np = np.array(list)
     array3 = array_np.reshape((3,3))
 
-    def calc_mean(a: np.array) -> list:
-        axis0 = np.mean(a, axis= 0).tolist()
-        axis1 = np.mean(a, axis= 1).tolist()
-        flat = np.mean(a)
+    def calculation(a: np.array, func_np) -> list:
+        axis0 = func_np(a, axis= 0).tolist()
+        axis1 = func_np(a, axis= 1).tolist()
+        flat = func_np(a)
 
         return [axis0, axis1, flat]
 
-    def calc_var(a: np.array) -> list:
-        axis0 = np.var(a, axis= 0).tolist()
-        axis1 = np.var(a, axis= 1).tolist()
-        flat = np.var(a)
-
-        return [axis0, axis1, flat]
-
-    def calc_std(a: np.array) -> list:
-        axis0 = np.std(a, axis= 0).tolist()
-        axis1 = np.std(a, axis= 1).tolist()
-        flat = np.std(a)
-
-        return [axis0, axis1, flat]
-
-    def calc_max(a: np.array) -> list:
-        axis0 = np.max(a, axis= 0).tolist()
-        axis1 = np.max(a, axis= 1).tolist()
-        flat = np.max(a)
-
-        return [axis0, axis1, flat]
-
-    def calc_min(a: np.array) -> list:
-        axis0 = np.min(a, axis= 0).tolist()
-        axis1 = np.min(a, axis= 1).tolist()
-        flat = np.min(a)
-
-        return [axis0, axis1, flat]
-
-    def calc_sum(a: np.array) -> list:
-        axis0 = np.sum(a, axis= 0).tolist()
-        axis1 = np.sum(a, axis= 1).tolist()
-        flat = np.sum(a)
-
-        return [axis0, axis1, flat]
-
-    calculation = {
-        'mean': calc_mean(array3)
-        ,'variance': calc_var(array3)
-        ,'standard deviation': calc_std(array3)
-        ,'max': calc_max(array3)
-        ,'min': calc_min(array3)
-        ,'sum': calc_sum(array3)
+    calculated = {
+        'mean': calculation(array3, func_np= np.mean)
+        ,'variance': calculation(array3, func_np= np.var)
+        ,'standard deviation': calculation(array3, func_np= np.std)
+        ,'max': calculation(array3, func_np= np.max)
+        ,'min': calculation(array3, func_np= np.min)
+        ,'sum': calculation(array3, func_np= np.sum)
     }
-    return calculation
+    return calculated
